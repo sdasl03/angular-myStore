006d4d062a419e1caa80069abb02ec2e84993add
sdasl03 <61993393+sdasl03@users.noreply.github.com>
2022-11-24 12:01:01 +0000

    2 files modified, 2 added, not working why?

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
+
  @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -9,8 +11,15 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-constructor(private route: ActivatedRoute) {}
-
+constructor(
+  private route: ActivatedRoute,
+  private cartService: CartService
+  ) {}
+  
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
 ngOnInit() {
   // First get the product id from the current route.
   const routeParams = this.route.snapshot.paramMap;
@@ -19,4 +28,5 @@ ngOnInit() {
   // Find the product that correspond with the id provided in route.
   this.product = products.find(product => product.id === productIdFromRoute);
 }
+
 }
